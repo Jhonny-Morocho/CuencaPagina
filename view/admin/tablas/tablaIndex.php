85968ae30c799d79ade453182cf7b021df4a13cb
Jhonny-Morocho <37280267+Jhonny-Morocho@users.noreply.github.com>
2020-10-09 16:08:42 -0500

    Restringir wiiggetes

--- view/admin/tablas/tablaIndex.php
+++ view/admin/tablas/tablaIndex.php
@@ -42,53 +42,55 @@ $formatoFechaActual=$añoActual."-".$mesActual."-01";
  <!-- Main content -->
 <section class="content">
 
-<div class="row">
-      <div class="col-lg-4 col-xs-6">
-        <!-- small box -->
-        <div class="small-box bg-aqua">
-          <div class="inner">
-            <h3><?php  echo $numFacturasMes?><sup style="font-size: 20px">#</sup></h3>
+  <div class="row">
+  <?php if($_SESSION['tipo_usuario']=="admin"){ ?>
+          <div class="col-lg-4 col-xs-6">
+            <!-- small box -->
+            <div class="small-box bg-aqua">
+              <div class="inner">
+                <h3><?php  echo $numFacturasMes?><sup style="font-size: 20px">#</sup></h3>
 
-            <p>New Orders in the month</p>
+                <p>New Orders in the month</p>
+              </div>
+              <div class="icon">
+                <i class="ion ion-bag"></i>
+              </div>
+              <!-- <a href="../view/admin/listarVentas.php" class="small-box-footer">More info <i class="fa fa-arrow-circle-right"></i></a> -->
+            </div>
           </div>
-          <div class="icon">
-            <i class="ion ion-bag"></i>
-          </div>
-          <!-- <a href="../view/admin/listarVentas.php" class="small-box-footer">More info <i class="fa fa-arrow-circle-right"></i></a> -->
-        </div>
-      </div>
 
-      <!-- ./col -->
-      <div class="col-lg-4 col-xs-6">
-        <!-- small box -->
-        <div class="small-box bg-yellow">
-          <div class="inner">
-            <h3><?php  echo $numClientesMes ?><sup style="font-size: 20px">#</sup></h3>
+          <!-- ./col -->
+          <div class="col-lg-4 col-xs-6">
+            <!-- small box -->
+            <div class="small-box bg-yellow">
+              <div class="inner">
+                <h3><?php  echo $numClientesMes ?><sup style="font-size: 20px">#</sup></h3>
 
-            <p>Registered users in the month </p>
+                <p>Registered users in the month </p>
+              </div>
+              <div class="icon">
+                <i class="ion ion-person-add"></i>
+              </div>
+              <!-- <a href="../view/admin/listarClientes.php" class="small-box-footer">More info <i class="fa fa-arrow-circle-right"></i></a> -->
+            </div>
           </div>
-          <div class="icon">
-            <i class="ion ion-person-add"></i>
-          </div>
-          <!-- <a href="../view/admin/listarClientes.php" class="small-box-footer">More info <i class="fa fa-arrow-circle-right"></i></a> -->
-        </div>
-      </div>
 
-      <div class="col-lg-4 col-xs-6">
-        <!-- small box -->
-        <div class="small-box bg-green">
-          <div class="inner">
-            <h3><?php  echo $totalVendidoMes ?><sup style="font-size: 20px">$</sup></h3>
+          <div class="col-lg-4 col-xs-6">
+            <!-- small box -->
+            <div class="small-box bg-green">
+              <div class="inner">
+                <h3><?php  echo $totalVendidoMes ?><sup style="font-size: 20px">$</sup></h3>
 
-            <p>Registered buy in the month </p>
+                <p>Registered buy in the month </p>
+              </div>
+              <div class="icon">
+                <i class="ion ion-stats-bars"></i>
+              </div>
+            </div>
           </div>
-          <div class="icon">
-            <i class="ion ion-stats-bars"></i>
-          </div>
-        </div>
-      </div>
-      <!-- ./col -->
-    </div>
+          <!-- ./col -->
+  <?php } ?>
+  </div>
 </section>
 <!-- /.content -->
 </div>
